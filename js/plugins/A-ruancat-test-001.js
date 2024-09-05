3deed39dd68dad38d5bff802709b1b1dbc664c62
ruan-cat <1219043956@qq.com>
2024-09-05 16:24:25 +0800

    🎉 init: const lodashES = await import("lodash-es");
    尝试动态导入模块；

--- js/plugins/A-ruancat-test-001.js
+++ js/plugins/A-ruancat-test-001.js
@@ -1,3 +1,13 @@
-(function main() {
+(async function main() {
   console.log(" 阮喵喵 IIFE 就绪 ");
+
+  const lodashES = await import("lodash-es");
+  const idPrefix = "ruancat-";
+
+  function getId() {
+    return lodashES.uniqueId(idPrefix);
+  }
+
+  const id = getId();
+  console.log(" show me id = ");
 })();
