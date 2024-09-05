5727175afd1a160a64da85286e8e5fab207f5d15
ruan-cat <1219043956@qq.com>
2024-09-05 16:36:55 +0800

    🤔 save-file: 尝试实现模块导入；

--- js/plugins/A-ruancat-test-001.js
+++ js/plugins/A-ruancat-test-001.js
@@ -1,7 +1,9 @@
+// import lodashES from "lodash-es";
+
 (async function main() {
   console.log(" 阮喵喵 IIFE 就绪 ");
 
-  const lodashES = await import("lodash-es");
+  // const lodashES = await import("lodash-es");
   const idPrefix = "ruancat-";
 
   function getId() {
