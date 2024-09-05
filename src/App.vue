2755a6220abfc8fc87528e0585f241220da5e0c7
ruan-cat <1219043956@qq.com>
2024-09-05 18:10:33 +0800

    🤔 save-file: 增加演示；

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
+  <section class="vite-vue-app-root">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <HelloWorld msg="Vite + Vue" />
+  </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.vite-vue-app-root {
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
