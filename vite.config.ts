45c624630336f548eff9435ca43e4be595497c26
ruan-cat <1219043956@qq.com>
2024-09-06 17:13:18 +0800

    ✨ feat: 完成模板迁移，不报错；

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,47 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/",
-	plugins: [vue()],
 
-	publicDir: false,
+	publicDir: "drill-project",
 
 	server: {
 		port: 8080,
 		open: true,
 	},
+
+	plugins: [
+		vue(),
+		createHtmlPlugin({
+			minify: false,
+			entry: "src/main.ts",
+			template: "drill-project/index.html",
+
+			/**
+			 * 需要注入 index.html ejs 模版的数据
+			 */
+			inject: {
+				data: {
+					// 出现在模版中的 <%- title %>
+					title: "插件集合示例",
+
+					// 出现在模版中的<%- injectScript %>
+					injectScript: `<script type="module" src="/src/main.ts"></script>`,
+				},
+
+				tags: [
+					{
+						injectTo: "body-prepend",
+						tag: "div",
+						attrs: {
+							id: "tag",
+						},
+					},
+				],
+			},
+		}),
+	],
 });
