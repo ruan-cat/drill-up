7ae1acb60771e996e802b1b0828422ae64345253
ruan-cat <1219043956@qq.com>
2024-09-10 11:12:45 +0800

    🎉 init: 初始化 element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#vue-root-app");
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.mount("#vue-root-app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { createHtmlPlugin } from "vite-plugin-html";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
@@ -16,6 +19,8 @@ export default defineConfig({
 
 	plugins: [
 		vue(),
+
+		// 重设index.html的入口 和 全局ts文件的注入
 		createHtmlPlugin({
 			minify: false,
 			entry: "../src/main.ts",
@@ -44,6 +49,14 @@ export default defineConfig({
 					},
 				],
 			},
+		}),
+
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+		}),
+
+		Components({
+			resolvers: [ElementPlusResolver()],
 		}),
 	],
 
