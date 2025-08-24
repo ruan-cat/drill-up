a029a2570c67403f1c17487f3ab343beb46f35a3
ruan-cat <1219043956@qq.com>
2025-08-24 17:38:14 +0800

    ✨ feat: 更新 pouch.json 和 project.registry.json 文件，调整状态历史和元数据时间戳，简化 rpg_core.js 文件，移除请求队列类实现，提升代码可维护性和资源管理能力。

---
+++ apps/gitee.jiumengjun.rmmv/sourceCodeFile/rpg_core/04 Point.js
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------------
+/**
+ * The point class.
+ *
+ * @class Point
+ * @constructor
+ * @param {Number} x The x coordinate
+ * @param {Number} y The y coordinate
+ */
+function Point() {
+    this.initialize.apply(this, arguments);
+}
+
+Point.prototype = Object.create(PIXI.Point.prototype);
+Point.prototype.constructor = Point;
+
+/**
+ * Initialize the point.
+ * 初始化点。
+ *
+ * @method initialize
+ * @param {Number} x - The x coordinate
+ * @param {Number} y - The y coordinate
+ */
+Point.prototype.initialize = function(x, y) {
+    PIXI.Point.call(this, x, y);
+};
+
+/**
+ * The x coordinate.
+ *
+ * @property x
+ * @type Number
+ */
+
+/**
+ * The y coordinate.
+ *
+ * @property y
+ * @type Number
+ */
