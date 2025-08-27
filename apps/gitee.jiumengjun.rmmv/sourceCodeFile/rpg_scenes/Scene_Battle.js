785139ee8def789a28968f4089a8babe969845f0
ruan-cat <1219043956@qq.com>
2025-08-27 08:21:17 +0800

    📃 docs(Scene_Battle): 为战斗场景中的方法添加 JSDoc 注释，提升代码可读性与一致性。

--- apps/gitee.jiumengjun.rmmv/sourceCodeFile/rpg_scenes/Scene_Battle.js
+++ apps/gitee.jiumengjun.rmmv/sourceCodeFile/rpg_scenes/Scene_Battle.js
@@ -29,6 +29,9 @@ Scene_Battle.prototype.constructor = Scene_Battle;
 /**
  * 初始化战斗场景
  * Initialize battle scene
+ *
+ * @memberof Scene_Battle
+ * @method initialize
  */
 Scene_Battle.prototype.initialize = function () {
 	Scene_Base.prototype.initialize.call(this);
@@ -37,6 +40,9 @@ Scene_Battle.prototype.initialize = function () {
 /**
  * 创建场景
  * Create scene
+ *
+ * @memberof Scene_Battle
+ * @method create
  */
 Scene_Battle.prototype.create = function () {
 	Scene_Base.prototype.create.call(this);
@@ -46,6 +52,9 @@ Scene_Battle.prototype.create = function () {
 /**
  * 开始场景
  * Start scene
+ *
+ * @memberof Scene_Battle
+ * @method start
  */
 Scene_Battle.prototype.start = function () {
 	Scene_Base.prototype.start.call(this);
@@ -57,6 +66,9 @@ Scene_Battle.prototype.start = function () {
 /**
  * 更新场景
  * Update scene
+ *
+ * @memberof Scene_Battle
+ * @method update
  */
 Scene_Battle.prototype.update = function () {
 	var active = this.isActive();
@@ -73,6 +85,9 @@ Scene_Battle.prototype.update = function () {
 /**
  * 更新战斗流程
  * Update battle process
+ *
+ * @memberof Scene_Battle
+ * @method updateBattleProcess
  */
 Scene_Battle.prototype.updateBattleProcess = function () {
 	if (!this.isAnyInputWindowActive() || BattleManager.isAborting() || BattleManager.isBattleEnd()) {
@@ -82,10 +97,12 @@ Scene_Battle.prototype.updateBattleProcess = function () {
 };
 
 /**
- * 是否有任何输入窗口处于活动状态
+ * 检查是否有任何输入窗口处于活动状态
  * Check if any input window is active
  *
- * @returns {boolean} 是否有活动的输入窗口 / Whether any input window is active
+ * @memberof Scene_Battle
+ * @method isAnyInputWindowActive
+ * @returns {Boolean} 是否有活动的输入窗口 - Whether any input window is active
  */
 Scene_Battle.prototype.isAnyInputWindowActive = function () {
 	return (
@@ -101,6 +118,9 @@ Scene_Battle.prototype.isAnyInputWindowActive = function () {
 /**
  * 改变输入窗口
  * Change input window
+ *
+ * @memberof Scene_Battle
+ * @method changeInputWindow
  */
 Scene_Battle.prototype.changeInputWindow = function () {
 	if (BattleManager.isInputting()) {
@@ -117,6 +137,9 @@ Scene_Battle.prototype.changeInputWindow = function () {
 /**
  * 停止场景
  * Stop scene
+ *
+ * @memberof Scene_Battle
+ * @method stop
  */
 Scene_Battle.prototype.stop = function () {
 	Scene_Base.prototype.stop.call(this);
@@ -133,6 +156,9 @@ Scene_Battle.prototype.stop = function () {
 /**
  * 结束场景
  * Terminate scene
+ *
+ * @memberof Scene_Battle
+ * @method terminate
  */
 Scene_Battle.prototype.terminate = function () {
 	Scene_Base.prototype.terminate.call(this);
@@ -144,10 +170,12 @@ Scene_Battle.prototype.terminate = function () {
 };
 
 /**
- * 是否需要缓慢淡出
+ * 检查是否需要缓慢淡出
  * Check if needs slow fade out
  *
- * @returns {boolean} 是否需要缓慢淡出 / Whether needs slow fade out
+ * @memberof Scene_Battle
+ * @method needsSlowFadeOut
+ * @returns {Boolean} 是否需要缓慢淡出 - Whether needs slow fade out
  */
 Scene_Battle.prototype.needsSlowFadeOut = function () {
 	return SceneManager.isNextScene(Scene_Title) || SceneManager.isNextScene(Scene_Gameover);
@@ -156,6 +184,9 @@ Scene_Battle.prototype.needsSlowFadeOut = function () {
 /**
  * 更新状态窗口
  * Update status window
+ *
+ * @memberof Scene_Battle
+ * @method updateStatusWindow
  */
 Scene_Battle.prototype.updateStatusWindow = function () {
 	if ($gameMessage.isBusy()) {
@@ -170,6 +201,9 @@ Scene_Battle.prototype.updateStatusWindow = function () {
 /**
  * 更新窗口位置
  * Update window positions
+ *
+ * @memberof Scene_Battle
+ * @method updateWindowPositions
  */
 Scene_Battle.prototype.updateWindowPositions = function () {
 	var statusX = 0;
@@ -195,6 +229,9 @@ Scene_Battle.prototype.updateWindowPositions = function () {
 /**
  * 创建显示对象
  * Create display objects
+ *
+ * @memberof Scene_Battle
+ * @method createDisplayObjects
  */
 Scene_Battle.prototype.createDisplayObjects = function () {
 	this.createSpriteset();
@@ -209,6 +246,9 @@ Scene_Battle.prototype.createDisplayObjects = function () {
 /**
  * 创建精灵组
  * Create spriteset
+ *
+ * @memberof Scene_Battle
+ * @method createSpriteset
  */
 Scene_Battle.prototype.createSpriteset = function () {
 	this._spriteset = new Spriteset_Battle();
@@ -218,6 +258,9 @@ Scene_Battle.prototype.createSpriteset = function () {
 /**
  * 创建所有窗口
  * Create all windows
+ *
+ * @memberof Scene_Battle
+ * @method createAllWindows
  */
 Scene_Battle.prototype.createAllWindows = function () {
 	this.createLogWindow();
@@ -233,19 +276,37 @@ Scene_Battle.prototype.createAllWindows = function () {
 	this.createScrollTextWindow();
 };
 
-/* 创建日志窗口 */
+/**
+ * 创建日志窗口
+ * Create log window
+ *
+ * @memberof Scene_Battle
+ * @method createLogWindow
+ */
 Scene_Battle.prototype.createLogWindow = function () {
 	this._logWindow = new Window_BattleLog();
 	this.addWindow(this._logWindow);
 };
 
-/* 创建状态窗口 */
+/**
+ * 创建状态窗口
+ * Create status window
+ *
+ * @memberof Scene_Battle
+ * @method createStatusWindow
+ */
 Scene_Battle.prototype.createStatusWindow = function () {
 	this._statusWindow = new Window_BattleStatus();
 	this.addWindow(this._statusWindow);
 };
 
-/* 创建队伍指令窗口 */
+/**
+ * 创建队伍指令窗口
+ * Create party command window
+ *
+ * @memberof Scene_Battle
+ * @method createPartyCommandWindow
+ */
 Scene_Battle.prototype.createPartyCommandWindow = function () {
 	this._partyCommandWindow = new Window_PartyCommand();
 	this._partyCommandWindow.setHandler("fight", this.commandFight.bind(this));
@@ -254,7 +315,13 @@ Scene_Battle.prototype.createPartyCommandWindow = function () {
 	this.addWindow(this._partyCommandWindow);
 };
 
-/* 创建角色指令窗口 */
+/**
+ * 创建角色指令窗口
+ * Create actor command window
+ *
+ * @memberof Scene_Battle
+ * @method createActorCommandWindow
+ */
 Scene_Battle.prototype.createActorCommandWindow = function () {
 	this._actorCommandWindow = new Window_ActorCommand();
 	this._actorCommandWindow.setHandler("attack", this.commandAttack.bind(this));
@@ -265,14 +332,26 @@ Scene_Battle.prototype.createActorCommandWindow = function () {
 	this.addWindow(this._actorCommandWindow);
 };
 
-/* 创建帮助窗口 */
+/**
+ * 创建帮助窗口
+ * Create help window
+ *
+ * @memberof Scene_Battle
+ * @method createHelpWindow
+ */
 Scene_Battle.prototype.createHelpWindow = function () {
 	this._helpWindow = new Window_Help();
 	this._helpWindow.visible = false;
 	this.addWindow(this._helpWindow);
 };
 
-/* 创建技能窗口 */
+/**
+ * 创建技能窗口
+ * Create skill window
+ *
+ * @memberof Scene_Battle
+ * @method createSkillWindow
+ */
 Scene_Battle.prototype.createSkillWindow = function () {
 	var wy = this._helpWindow.y + this._helpWindow.height;
 	var wh = this._statusWindow.y - wy;
@@ -283,7 +362,13 @@ Scene_Battle.prototype.createSkillWindow = function () {
 	this.addWindow(this._skillWindow);
 };
 
-/* 创建物品窗口 */
+/**
+ * 创建物品窗口
+ * Create item window
+ *
+ * @memberof Scene_Battle
+ * @method createItemWindow
+ */
 Scene_Battle.prototype.createItemWindow = function () {
 	var wy = this._helpWindow.y + this._helpWindow.height;
 	var wh = this._statusWindow.y - wy;
@@ -294,7 +379,13 @@ Scene_Battle.prototype.createItemWindow = function () {
 	this.addWindow(this._itemWindow);
 };
 
-/* 创建角色窗口 */
+/**
+ * 创建角色窗口
+ * Create actor window
+ *
+ * @memberof Scene_Battle
+ * @method createActorWindow
+ */
 Scene_Battle.prototype.createActorWindow = function () {
 	this._actorWindow = new Window_BattleActor(0, this._statusWindow.y);
 	this._actorWindow.setHandler("ok", this.onActorOk.bind(this));
@@ -302,7 +393,13 @@ Scene_Battle.prototype.createActorWindow = function () {
 	this.addWindow(this._actorWindow);
 };
 
-/* 创建敌人窗口 */
+/**
+ * 创建敌人窗口
+ * Create enemy window
+ *
+ * @memberof Scene_Battle
+ * @method createEnemyWindow
+ */
 Scene_Battle.prototype.createEnemyWindow = function () {
 	this._enemyWindow = new Window_BattleEnemy(0, this._statusWindow.y);
 	this._enemyWindow.x = Graphics.boxWidth - this._enemyWindow.width;
@@ -311,7 +408,13 @@ Scene_Battle.prototype.createEnemyWindow = function () {
 	this.addWindow(this._enemyWindow);
 };
 
-/* 创建信息窗口 */
+/**
+ * 创建信息窗口
+ * Create message window
+ *
+ * @memberof Scene_Battle
+ * @method createMessageWindow
+ */
 Scene_Battle.prototype.createMessageWindow = function () {
 	this._messageWindow = new Window_Message();
 	this.addWindow(this._messageWindow);
@@ -320,18 +423,36 @@ Scene_Battle.prototype.createMessageWindow = function () {
 	}, this);
 };
 
-/* 创建滚动文本窗口 */
+/**
+ * 创建滚动文本窗口
+ * Create scroll text window
+ *
+ * @memberof Scene_Battle
+ * @method createScrollTextWindow
+ */
 Scene_Battle.prototype.createScrollTextWindow = function () {
 	this._scrollTextWindow = new Window_ScrollText();
 	this.addWindow(this._scrollTextWindow);
 };
 
-/* 刷新状态 */
+/**
+ * 刷新状态
+ * Refresh status
+ *
+ * @memberof Scene_Battle
+ * @method refreshStatus
+ */
 Scene_Battle.prototype.refreshStatus = function () {
 	this._statusWindow.refresh();
 };
 
-/* 开始队伍指令选择 */
+/**
+ * 开始队伍指令选择
+ * Start party command selection
+ *
+ * @memberof Scene_Battle
+ * @method startPartyCommandSelection
+ */
 Scene_Battle.prototype.startPartyCommandSelection = function () {
 	this.refreshStatus();
 	this._statusWindow.deselect();
@@ -340,31 +461,61 @@ Scene_Battle.prototype.startPartyCommandSelection = function () {
 	this._partyCommandWindow.setup();
 };
 
-/* 战斗的指令 */
+/**
+ * 战斗指令
+ * Fight command
+ *
+ * @memberof Scene_Battle
+ * @method commandFight
+ */
 Scene_Battle.prototype.commandFight = function () {
 	this.selectNextCommand();
 };
 
-/* 逃跑的指令 */
+/**
+ * 逃跑指令
+ * Escape command
+ *
+ * @memberof Scene_Battle
+ * @method commandEscape
+ */
 Scene_Battle.prototype.commandEscape = function () {
 	BattleManager.processEscape();
 	this.changeInputWindow();
 };
 
-/* 开始角色指令选择 */
+/**
+ * 开始角色指令选择
+ * Start actor command selection
+ *
+ * @memberof Scene_Battle
+ * @method startActorCommandSelection
+ */
 Scene_Battle.prototype.startActorCommandSelection = function () {
 	this._statusWindow.select(BattleManager.actor().index());
 	this._partyCommandWindow.close();
 	this._actorCommandWindow.setup(BattleManager.actor());
 };
 
-/* 攻击的指令 */
+/**
+ * 攻击指令
+ * Attack command
+ *
+ * @memberof Scene_Battle
+ * @method commandAttack
+ */
 Scene_Battle.prototype.commandAttack = function () {
 	BattleManager.inputtingAction().setAttack();
 	this.selectEnemySelection();
 };
 
-/* 技能的指令 */
+/**
+ * 技能指令
+ * Skill command
+ *
+ * @memberof Scene_Battle
+ * @method commandSkill
+ */
 Scene_Battle.prototype.commandSkill = function () {
 	this._skillWindow.setActor(BattleManager.actor());
 	this._skillWindow.setStypeId(this._actorCommandWindow.currentExt());
@@ -373,39 +524,75 @@ Scene_Battle.prototype.commandSkill = function () {
 	this._skillWindow.activate();
 };
 
-/* 防御的指令 */
+/**
+ * 防御指令
+ * Guard command
+ *
+ * @memberof Scene_Battle
+ * @method commandGuard
+ */
 Scene_Battle.prototype.commandGuard = function () {
 	BattleManager.inputtingAction().setGuard();
 	this.selectNextCommand();
 };
 
-/* 物品的指令 */
+/**
+ * 物品指令
+ * Item command
+ *
+ * @memberof Scene_Battle
+ * @method commandItem
+ */
 Scene_Battle.prototype.commandItem = function () {
 	this._itemWindow.refresh();
 	this._itemWindow.show();
 	this._itemWindow.activate();
 };
 
-/* 选择下一个指令 */
+/**
+ * 选择下一个指令
+ * Select next command
+ *
+ * @memberof Scene_Battle
+ * @method selectNextCommand
+ */
 Scene_Battle.prototype.selectNextCommand = function () {
 	BattleManager.selectNextCommand();
 	this.changeInputWindow();
 };
 
-/* 选择上一个指令 */
+/**
+ * 选择上一个指令
+ * Select previous command
+ *
+ * @memberof Scene_Battle
+ * @method selectPreviousCommand
+ */
 Scene_Battle.prototype.selectPreviousCommand = function () {
 	BattleManager.selectPreviousCommand();
 	this.changeInputWindow();
 };
 
-/* 选择可选择的角色 */
+/**
+ * 选择角色
+ * Select actor selection
+ *
+ * @memberof Scene_Battle
+ * @method selectActorSelection
+ */
 Scene_Battle.prototype.selectActorSelection = function () {
 	this._actorWindow.refresh();
 	this._actorWindow.show();
 	this._actorWindow.activate();
 };
 
-/* 当角色确定 */
+/**
+ * 当角色确定
+ * On actor ok
+ *
+ * @memberof Scene_Battle
+ * @method onActorOk
+ */
 Scene_Battle.prototype.onActorOk = function () {
 	var action = BattleManager.inputtingAction();
 	action.setTarget(this._actorWindow.index());
@@ -415,7 +602,13 @@ Scene_Battle.prototype.onActorOk = function () {
 	this.selectNextCommand();
 };
 
-/* 当角色取消 */
+/**
+ * 当角色取消
+ * On actor cancel
+ *
+ * @memberof Scene_Battle
+ * @method onActorCancel
+ */
 Scene_Battle.prototype.onActorCancel = function () {
 	this._actorWindow.hide();
 	switch (this._actorCommandWindow.currentSymbol()) {
@@ -430,7 +623,13 @@ Scene_Battle.prototype.onActorCancel = function () {
 	}
 };
 
-/* 选择可选择的敌人 */
+/**
+ * 选择敌人
+ * Select enemy selection
+ *
+ * @memberof Scene_Battle
+ * @method selectEnemySelection
+ */
 Scene_Battle.prototype.selectEnemySelection = function () {
 	this._enemyWindow.refresh();
 	this._enemyWindow.show();
@@ -438,7 +637,13 @@ Scene_Battle.prototype.selectEnemySelection = function () {
 	this._enemyWindow.activate();
 };
 
-/* 当敌人确定 */
+/**
+ * 当敌人确定
+ * On enemy ok
+ *
+ * @memberof Scene_Battle
+ * @method onEnemyOk
+ */
 Scene_Battle.prototype.onEnemyOk = function () {
 	var action = BattleManager.inputtingAction();
 	action.setTarget(this._enemyWindow.enemyIndex());
@@ -448,7 +653,13 @@ Scene_Battle.prototype.onEnemyOk = function () {
 	this.selectNextCommand();
 };
 
-/* 当敌人取消 */
+/**
+ * 当敌人取消
+ * On enemy cancel
+ *
+ * @memberof Scene_Battle
+ * @method onEnemyCancel
+ */
 Scene_Battle.prototype.onEnemyCancel = function () {
 	this._enemyWindow.hide();
 	switch (this._actorCommandWindow.currentSymbol()) {
@@ -466,7 +677,13 @@ Scene_Battle.prototype.onEnemyCancel = function () {
 	}
 };
 
-/* 当技能确定 */
+/**
+ * 当技能确定
+ * On skill ok
+ *
+ * @memberof Scene_Battle
+ * @method onSkillOk
+ */
 Scene_Battle.prototype.onSkillOk = function () {
 	var skill = this._skillWindow.item();
 	var action = BattleManager.inputtingAction();
@@ -475,13 +692,25 @@ Scene_Battle.prototype.onSkillOk = function () {
 	this.onSelectAction();
 };
 
-/* 当技能取消 */
+/**
+ * 当技能取消
+ * On skill cancel
+ *
+ * @memberof Scene_Battle
+ * @method onSkillCancel
+ */
 Scene_Battle.prototype.onSkillCancel = function () {
 	this._skillWindow.hide();
 	this._actorCommandWindow.activate();
 };
 
-/* 当物品确定 */
+/**
+ * 当物品确定
+ * On item ok
+ *
+ * @memberof Scene_Battle
+ * @method onItemOk
+ */
 Scene_Battle.prototype.onItemOk = function () {
 	var item = this._itemWindow.item();
 	var action = BattleManager.inputtingAction();
@@ -490,13 +719,25 @@ Scene_Battle.prototype.onItemOk = function () {
 	this.onSelectAction();
 };
 
-/* 当物品取消 */
+/**
+ * 当物品取消
+ * On item cancel
+ *
+ * @memberof Scene_Battle
+ * @method onItemCancel
+ */
 Scene_Battle.prototype.onItemCancel = function () {
 	this._itemWindow.hide();
 	this._actorCommandWindow.activate();
 };
 
-/* 当选择行动 */
+/**
+ * 当选择行动
+ * On select action
+ *
+ * @memberof Scene_Battle
+ * @method onSelectAction
+ */
 Scene_Battle.prototype.onSelectAction = function () {
 	var action = BattleManager.inputtingAction();
 	this._skillWindow.hide();
@@ -510,7 +751,13 @@ Scene_Battle.prototype.onSelectAction = function () {
 	}
 };
 
-/* 结束指令选择 */
+/**
+ * 结束指令选择
+ * End command selection
+ *
+ * @memberof Scene_Battle
+ * @method endCommandSelection
+ */
 Scene_Battle.prototype.endCommandSelection = function () {
 	this._partyCommandWindow.close();
 	this._actorCommandWindow.close();
